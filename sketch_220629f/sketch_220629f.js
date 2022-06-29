c73e9d62b15c5433440f545cc11724b7cca24773
MCUYeeZhiHua <79628297+MCUYeeZhiHua@users.noreply.github.com>
2022-06-29 16:06:35 +0800

    Add files via upload

---
+++ sketch_220629f/sketch_220629f.js
@@ -0,0 +1,15 @@
+//要把 Processing 的 pde 程式
+//翻譯成 p5.js JavaScript
+//(0)左邊程式先寫好
+//(1)到 pd2js.herokuapp.com 翻譯
+//(2)貼在這裡
+//(3)測試時 ，Run
+function setup() {
+  initializeFields();
+  create
+}
+
+
+function draw() {
+
+}
